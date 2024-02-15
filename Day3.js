807234fdeba78828f8ba0a3216026ea14462b2de
T Shivani <96899982+shivani-tripurari@users.noreply.github.com>
2024-02-15 19:37:10 +0530

    uploading day3

---
+++ Day3.js
@@ -0,0 +1,29 @@
+/*Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+
+toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
+notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".*/
+
+//Approach1
+
+var expect = function(value){
+    var obj = {
+        toBe: function(expectedVal){
+            if(expectedVal === value){
+                return true;
+            }
+            else{
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe: function(expectedVal){
+            if(expectedVal !== value){
+                return true;
+            }
+            else{
+                throw new Error("Equal");
+            }
+        }
+    }
+    return obj;
+}
+
